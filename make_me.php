de8564c56ec1ca6cdaa04c44b879c630998464f6
Kira94 <thenewdawn1994@hotmail.com>
2014-09-17 15:20:00 +0500

    My first commit

---
+++ make_me.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<style>
+	#change_me {
+	position: absolute;
+	top: 100px;
+	left: 400px;
+	font: 24px arial;}
+	#move_up #move_down #color #disappear {
+	padding: 5px;}
+</style>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<title>Untitled Document</title>
+</head>
+
+<body>
+<button id="move_up">Move Up</button>
+<button id="move_down">Move Down</button>
+<button id="color">Change Color</button>
+<button id="disappear">Disappear/Re-appear</button>
+<div id="change_me">Make Me Do Stuff!</div>
+<script>
+$(document).ready(function() {
+$("#move_up").click( function() {
+$("#change_me").animate({top:30},200);
+});//end move_up
+$("#move_down").click( function() {
+$("#change_me").animate({top:500},2000);
+});//end move_down
+$("#color").click( function() {
+$("#change_me").css("color", "purple");
+});//end color
+$("#disappear").click( function() {
+$("#change_me").toggle("slow");
+});//end disappear
+});//end doc ready
+</script>
+</body>
+</html>
